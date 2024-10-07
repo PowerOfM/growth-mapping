7475b23c34bc13ca8b852122aae275539d59cc73
Mesbah <5808728+PowerOfM@users.noreply.github.com>
2024-10-06 23:20:02 -0700

    draw

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -1,15 +1,36 @@
-import * as L from "leaflet"
-import { MapContainer, TileLayer } from "react-leaflet"
+import * as L from 'leaflet'
+import { useState } from 'react'
+import { MapContainer, Polygon, TileLayer, useMapEvent } from 'react-leaflet'
 
 // const initialBounds = L.latLngBounds([24, -126], [50, -66])
 const initialView: L.LatLngExpression = { lat: 49.1895166, lng: -123.004237 }
+
+// const latlngs: L.LatLngTuple[] = [
+//   [37, -109.05],
+//   [41, -109.03],
+//   [41, -102.05],
+//   [37, -102.04],
+// ]
+
+// <Polygon pathOptions={purpleOptions} positions={polygon} />
+const purpleOptions = { color: 'purple' }
+
+const MapHandler = () => {
+  const [latlngs, setLatlngs] = useState<L.LatLng[]>([])
+
+  const map = useMapEvent('click', (event: L.LeafletMouseEvent) => {
+    setLatlngs((prev) => [...prev, event.latlng])
+  })
+
+  return <Polygon pathOptions={purpleOptions} positions={latlngs} />
+}
 
 export function Map() {
   return (
     <MapContainer
       center={initialView}
       zoom={11}
-      style={{ height: "100vh", width: "100%" }}
+      style={{ height: '100vh', width: '100%' }}
     >
       <TileLayer
         attribution='&copy;<a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>, &copy;<a href="https://carto.com/attributions" target="_blank">CARTO</a>'
@@ -17,6 +38,15 @@ export function Map() {
         subdomains="abcd"
         maxZoom={14}
       />
+
+      <MapHandler />
     </MapContainer>
   )
 }
+
+// Vancouver neighbourhoods
+// https://opendata.vancouver.ca/explore/dataset/local-area-boundary/api/?disjunctive.name&location=12,49.24741,-123.12429
+
+// Full draw/edit
+// https://leaflet.github.io/Leaflet.draw/docs/examples/full.html
+// https://github.com/alex3165/react-leaflet-draw
